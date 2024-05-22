f0396ebcc0265005322f509285178fa95b31f084
20210770 <punnlert@kaist.ac.kr>
2024-05-22 22:42:56 +0900

    minor change

--- inti-site/src/App.jsx
+++ inti-site/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { io } from 'socket.io-client';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const socket = io('http://localhost:3000');
@@ -11,7 +9,7 @@ function App() {
   const [displayText, setDisplayText] = useState("Not Connected");
 
   socket.on('connect', (arg) => {
-    setData("connected with the server");
+    setDisplayText("connected with the server");
     console.log('connected with the server');
   })
 
