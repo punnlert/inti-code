bd9fe5275e6fba317fbdea8d3d2b260fa6735937
20210770 <punnlert@kaist.ac.kr>
2024-06-04 07:45:49 +0900

    basic element added

--- inti-site/src/App.jsx
+++ inti-site/src/App.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client';
+import { ChildList } from './components/ChildList.jsx';
 import './App.css'
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 
 function App() {
-  const [count, setCount] = useState(0);
   const [displayText, setDisplayText] = useState("Not Connected");
 
-  socket.on('connect', (arg) => {
-    setDisplayText("connected with the server");
-    console.log('connected with the server');
-  })
+  // socket.on('connect', (arg) => {
+  //   setDisplayText("connected with the server");
+  //   console.log('connected with the server');
+  // })
 
-  socket.on('button data', (data) => {
-    setDisplayText(data);
-    const input = JSON.parse(data);
-    console.log(input.Event);
-    //console.log(data);
-  })
+  // socket.on('button data', (data) => {
+  //   setDisplayText(data);
+  //   const input = JSON.parse(data);
+  //   console.log(input.Event);
+  // })
 
   return (
     <>
-      <div className="card">
-          <h1> {displayText} </h1>
+      <div className='appcontainer'>
+        <div className="navbarcontainer">
+
+        </div>
+        <div className="contentcontainer">
+          <div className="childlistcontainer">
+            <h2>Abused Children List</h2>
+            {/* <div className="tablecontainer">
+
+            </div> */}
+            <ChildList CaseManagement={null} Name={null} Sex={null} DOB={null} Status={null}/>
+          </div>
+        </div>
       </div>
     </>
   )
